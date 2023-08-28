767367451ed97c07f557cfb36f9f11cedef0f5f5
brunstighanhval <113671720+brunstighanhval@users.noreply.github.com>
2023-08-28 12:30:56 +0200

    ads
    asd

---
+++ infrastructure/Repository.cs
@@ -0,0 +1,64 @@
+﻿using System.Runtime.CompilerServices;
+using Dapper;
+using Npgsql;
+using Tests;
+using Book = infrastructure.DataModels.Book;
+
+namespace infrastructure;
+
+public class Repository
+{
+    private readonly NpgsqlDataSource _dataSource;
+
+    public Repository(NpgsqlDataSource dataSource)
+    {
+        _dataSource = dataSource;
+    }
+
+    public IEnumerable<Book> GetAllBooks()
+    {
+        var sql = $@"select * from library.books;";
+        using(var conn = _dataSource.OpenConnection())
+        {
+            return conn.Query<Book>(sql);
+        }
+    }
+
+
+    public Book CreateBook(string title, string publisher, string coverImgUrl)
+    {
+        var sql = $@"INSERT INTO library.books (title, publisher, coverimgurl)
+                VALUES (@title, @publisher, @coverImgUrl)
+                RETURNING *;";
+
+        using (var conn = _dataSource.OpenConnection())
+        {
+            return conn.QueryFirst<Book>(sql, new{title, publisher, coverImgUrl});
+        }
+    }
+    public Book UpdateBook(int id, string title, string publisher, string coverImgUrl)
+    {
+        var sql = @"UPDATE library.books 
+        SET title =@title,
+            publisher = @publisher,
+            coverimgurl = @coverImgUrl
+            WHERE bookid = @id
+            RETURNING *;";
+        using (var conn = _dataSource.OpenConnection())
+        {
+            return conn.QueryFirst<Book>(sql, new{id, title, publisher, coverImgUrl});
+        }
+    }
+    public bool DeleteBook(int id)
+    {
+        var sql = $@"
+        DELETE FROM library.books WHERE bookId = @id";
+        using (var conn = _dataSource.OpenConnection())
+        {
+            return conn.Execute(sql, new { id }) == 1;
+        }
+        
+    }
+
+
+}
---
+++ service/Service.cs
@@ -0,0 +1,61 @@
+﻿using infrastructure;
+using infrastructure.DataModels;
+
+namespace service;
+
+public class Service
+{
+    private readonly Repository _repository;
+
+    public Service(Repository repository)
+    {
+        _repository = repository;
+    }
+
+    public IEnumerable<Book> GetAllBooks()
+    {
+        try
+        {
+            return _repository.GetAllBooks();
+        }
+        catch (Exception)
+        {
+            throw new Exception("Could not get books");
+        }
+    }
+
+    public Book CreateBook(string title, string publisher, string coverImgUrl)
+    {
+        try
+        {
+            return _repository.CreateBook(title, publisher, coverImgUrl);
+        }
+        catch (Exception )
+        {
+            throw new Exception("Could not create new book");
+        }
+    }
+    public Book UpdateBook(int id, string title, string publisher, string coverImgUrl)
+    {
+        try
+        {
+            return _repository.UpdateBook(id, title, publisher, coverImgUrl);
+        }
+        catch (Exception)
+        {
+            throw new Exception("could not update the book");
+        }
+    }
+    public bool DeleteBook(int id)
+    {
+        try
+        {
+            return _repository.DeleteBook(id);
+        }
+        catch (Exception e)
+        {
+            throw new Exception("could not delete the book");
+        }
+    }
+
+}
